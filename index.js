067450b1b46505f3cc730e34a5718f1d815311b0
Sam Shearer <sshearer101@gmail.com>
2021-08-06 12:58:35 -0600

    commit tree b116fd0b58fb116f762a96b2e6a7f0fa85369760

--- index.js
+++ index.js
@@ -1,1 +1,83 @@
-// Your code here
+function saturdayFun(activity=`roller-skate`){
+    return `This Saturday, I want to ${activity}!`
+}
+
+saturdayFun('bathe my dog')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function saturdayFun (activity = `roller-skate`){
+//     return `This Saturday, I want to ${activity}!`
+//     }
+
+
+// const mondayWork = function(activity = 'go to the office') {
+//     return `This Monday, I will ${activity}.`
+// }
+
+
+
+// function wrapAdjective(thing='*'){
+//     return function(word = 'nice'){
+//         return `You are ${thing}${word}${thing}!`;
+//     }
+// }
+// const callFunction = wrapAdjective('*')('a dedicated programmer')
+
+
+
+
+// function loopThroughAndFind(position, finder){
+//     let result = null
+//     for(let item of position){
+//         if(finder(item) === true)
+//             result = item
+//             break 
+//     }
+// }
+// return result
+
+// function isTeam(team) {
+//     return team.name === 'Newcastle United'
+// }
+
+
+
+// let updatedTeamList = teams.map(function(item){
+//     item.position = (item.position+1)
+//     return item
+// })
+// console.log(updatedTeamList)
+ 
+
+// function updatePosition(position, updateTeam) {
+//     let updatedNewPosition = []
+//     for (let item of position) {
+//         console.log(updateTeam(item))
+//     }
+// }
+
+// function newPosition (item){
+//     item.position = (item.position+1)
+//     return item
+// }
